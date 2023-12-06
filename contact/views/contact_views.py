7105f1b517084eae4653b561655341697e924055
‘Pablo <ribeirojacobsenn@gmail.com>
2023-12-05 22:50:02 -0300

    Paginação

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.http import Http404
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[10:20]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -52,10 +57,15 @@ def search(request):
         Q(last_name__icontains=search_value) |
         Q(phone__icontains=search_value) |
         Q(email__icontains=search_value)).order_by('-id')
+    
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
