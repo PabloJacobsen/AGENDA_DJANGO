2fb55712d8591e7a886ab1eb3651b660f22a85d8
‘Pablo <ribeirojacobsenn@gmail.com>
2023-12-12 23:38:10 -0300

    ‘UserLogin’

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,17 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept':'image/*',
+            }
+        )
+    )
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -20,11 +29,50 @@ class ContactForm(forms.ModelForm):
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
+            'picture',
         )
         widgets = {
             ...
         }
 
+
     def clean(self):
+        # cleaned_data = self.cleaned_data
         cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                    'first_name = last_name',
+                    code='invalid'
+                )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
         return super().clean()
+    
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username',
+        )
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('E-Mail já cadastrado', conde='invalid')
+            )
