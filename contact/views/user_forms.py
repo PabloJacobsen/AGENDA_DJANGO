125c086e52074a613c47989af6b4d3af887d7426
‘Pablo <ribeirojacobsenn@gmail.com>
2023-12-14 00:26:58 -0300

    ‘Owner’

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -3,6 +3,7 @@ from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     form = RegisterForm()
@@ -20,6 +21,7 @@ def register(request):
         }
     )
 
+@login_required(login_url='contact:login')
 def user_update(request):
     form = RegisterUpdateForm(instance=request.user)
     if request.method != 'POST':
@@ -58,6 +60,7 @@ def login_view(request):
         }
     )
 
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
